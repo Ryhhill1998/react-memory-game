f8e8d7648eb3ba8447891f6de916901becb9aa74
ryhhill1998 <ryhhill1998@outlook.com>
2022-10-17 16:28:50 +0100

    added card flip animation and choice checking functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import Card from "./components/card/card.component";
 import "./App.css";
 
 const cardImages = [
-  { src: "public/img/helmet-1.png" },
-  { src: "public/img/potion-1.png" },
-  { src: "public/img/ring-1.png" },
-  { src: "public/img/scroll-1.png" },
-  { src: "public/img/shield-1.png" },
-  { src: "public/img/sword-1.png" },
+  { src: "img/helmet-1.png" },
+  { src: "img/potion-1.png" },
+  { src: "img/ring-1.png" },
+  { src: "img/scroll-1.png" },
+  { src: "img/shield-1.png" },
+  { src: "img/sword-1.png" },
 ];
 
 const App = () => {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages].map((card) => ({
       ...card,
       id: Math.random(),
+      matched: false,
     }));
 
     // fisher-yates algorithm
@@ -37,12 +41,57 @@ const App = () => {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // handle user choice
+  const handleChoice = (card) => {
+    console.log("Card chosen");
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare selected cards
+  useEffect(() => {
+    if (!choiceOne || !choiceTwo) return;
+
+    console.log(choiceOne);
+    console.log(choiceTwo);
+
+    if (choiceOne.src !== choiceTwo.src) {
+      setTimeout(() => resetTurn(), 1000);
+      return;
+    }
+
+    const updatedCards = cards.map((card) => {
+      if (card.src === choiceOne.src || card.src === choiceTwo.src) {
+        return { ...card, matched: true };
+      }
+      return card;
+    });
+
+    setCards(updatedCards);
+
+    resetTurn();
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices and increment turns by 1
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(turns + 1);
+  };
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import "./card.styles.css";
+
+const Card = ({ card, handleChoice, flipped }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          onClick={handleClick}
+          src="img/cover.png"
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
