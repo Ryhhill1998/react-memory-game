df0cd433b157b1055843bc098e6c471bfbdbb81a
ryhhill1998 <ryhhill1998@outlook.com>
2022-10-17 11:04:45 +0100

    added basic CSS and shuffle cards functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import { useState } from "react";
+
 import "./App.css";
 
-function App() {
+const cardImages = [
+  { src: "public/img/helmet-1.png" },
+  { src: "public/img/potion-1.png" },
+  { src: "public/img/ring-1.png" },
+  { src: "public/img/scroll-1.png" },
+  { src: "public/img/shield-1.png" },
+  { src: "public/img/sword-1.png" },
+];
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].map((card) => ({
+      ...card,
+      id: Math.random(),
+    }));
+
+    // fisher-yates algorithm
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = shuffledCards[i];
+      shuffledCards[i] = shuffledCards[j];
+      shuffledCards[j] = temp;
+    }
+
+    // set cards to shuffled cards
+    setCards(shuffledCards);
+
+    // reset turns
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
-}
+};
 
 export default App;
